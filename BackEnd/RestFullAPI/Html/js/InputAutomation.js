120cf251da2d36c69da2ec682b5e7ff9ea96e196
Rafael Tonello <rafinha_tonello@localhost.localdomain>
2018-04-14 23:36:24 -0300

    Start html interface

---
+++ BackEnd/RestFullAPI/Html/js/InputAutomation.js
@@ -0,0 +1,47 @@
+﻿function InputAutomation(element)
+{
+    //if element does not contains the "aut-hint" attribute, creates this
+    if (typeof(element.getAttribute("aut-hint")) =="undefined")
+        element.setAttribute("aut-hint", "");
+
+    if (element.getAttribute("aut-hint") != "")
+    {
+        element.addEventListener("focus", function(){
+            if (element.value == element.getAttribute("aut-hint"))
+            {
+                element.value = "";
+                element.className = element.className.replace(/hinted/g, "");
+            }
+          
+        });
+
+        element.addEventListener("blur", function(){
+            if (element.value == "")
+            {
+                element.value =element.getAttribute("aut-hint");
+                element.className += " hinted";
+            }
+        
+        });
+    }
+
+
+    if (element.value == "")
+    {
+        element.value =element.getAttribute("aut-hint");
+        element.className += " hinted";
+    }
+
+}
+
+
+
+//this class looks byt inputs, on html, with property automation="InputAutomation"
+window.addEventListener("load", function(){
+    inputs = document.querySelectorAll("input[automation=InputAutomation]");
+
+    for (var cont = 0; cont < inputs.length; cont++)
+    {
+        new InputAutomation(inputs[cont]);
+    }
+});
